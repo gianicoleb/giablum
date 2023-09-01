bf06fba8b368e9750962947c11ec00ae9af2f5e2
gianicoleb <98177363+gianicoleb@users.noreply.github.com>
2023-09-01 13:03:01 -0700

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -8,3 +8,20 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const intro = document.querySelector('.intro');
+    const mainContent = document.querySelector('.main-content');
+
+    // Wait for 5 seconds (2s for delay + 3s for animation duration)
+    setTimeout(() => {
+        // Fade out the intro
+        intro.style.opacity = '0';
+
+        // After the fade out transition is done, hide the intro and show the main content
+        intro.addEventListener('transitionend', () => {
+            intro.style.display = 'none';
+            mainContent.style.display = 'block';
+        });
+    }, 2000);
+});
